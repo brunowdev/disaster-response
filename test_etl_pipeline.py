1a32dcda7e9a4fc6a1c1a22da7d0e3c83995a135
Bruno Bitencourt Luiz <obrunobitencourt@gmail.com>
2020-01-03 22:22:57 -0300

    Update etl tests

--- test_etl_pipeline.py
+++ test_etl_pipeline.py
@@ -1,4 +1,6 @@
-import etl_pipeline as etl
+import process_data as etl
+
+import pandas as pd
 
 # Sample string from the original dataset
 valid_categories_sample_text = 'related-1;request-0;offer-0;aid_related-0;medical_help-0;medical_products-0;search_and_rescue-0;security-0;military-0;child_alone-0;water-0;food-0;shelter-0;clothing-0;money-0;missing_people-0;refugees-0;death-0;other_aid-0;infrastructure_related-0;transport-0;buildings-0;electricity-0;tools-0;hospitals-0;shops-0;aid_centers-0;other_infrastructure-0;weather_related-0;floods-0;storm-0;fire-0;earthquake-0;cold-0;other_weather-0;direct_report-0'
@@ -20,3 +22,21 @@ def test_parse_column_values():
     
 def test_parse_column_values_length():
     assert len(etl.get_categories_value_from_text(valid_categories_sample_text)) == etl.supported_categories
+    
+def test_parse_dataframe_columns_with_values():
+    
+    df = pd.DataFrame({ 
+            'id' : [10],
+            'categories' : ['related-1;request-0;offer-0']
+        })
+
+    expected_categories = [ 'related', 'request', 'offer']
+    expected_categories_values = [1, 0, 0]
+    expected_columns = ['id', 'categories'] + expected_categories
+    
+    updated_df = etl.expand_categories_to_columns_with_values(df, 'categories', expected_categories)
+    
+    assert list(updated_df.columns) == expected_columns
+    assert updated_df.related.max() == 1
+    assert updated_df.request.max() == 0
+    assert updated_df.offer.max() == 0
